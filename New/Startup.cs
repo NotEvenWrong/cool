efd4ee3019a793cfd346c86273bb3c85aa1970d3
wrong <wrong@not.com>
2018-07-04 23:37:31 +0800

    1

--- New/Startup.cs
+++ New/Startup.cs
@@ -29,6 +29,7 @@ namespace New
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            //unbeliveable
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
